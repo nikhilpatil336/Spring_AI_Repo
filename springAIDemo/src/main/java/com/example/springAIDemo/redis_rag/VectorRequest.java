178019a0432ed78e666743cd436582e091edf7d1
Nikhil Patil <nikhil.neosoft@Sklforest.com>
2025-09-22 13:20:26 +0530

    unwanted redis rag code commented

--- springAIDemo/src/main/java/com/example/springAIDemo/redis_rag/VectorRequest.java
+++ springAIDemo/src/main/java/com/example/springAIDemo/redis_rag/VectorRequest.java
@@ -1,13 +1,13 @@
-package com.example.springAIDemo.redis_rag;
-
-public class VectorRequest {
-    private String id;
-    private String content;
-
-    // Getters and setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-}
+//package com.example.springAIDemo.redis_rag;
+//
+//public class VectorRequest {
+//    private String id;
+//    private String content;
+//
+//    // Getters and setters
+//    public String getId() { return id; }
+//    public void setId(String id) { this.id = id; }
+//
+//    public String getContent() { return content; }
+//    public void setContent(String content) { this.content = content; }
+//}
